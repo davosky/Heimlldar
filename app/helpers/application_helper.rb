ada3ace94332f52bae028ecd1ce16c70570a4ca6
Davo Davosky <davodavosky@gmail.com>
2022-03-13 09:46:22 +0100

    Changed absence and disease routines in helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -22,40 +22,14 @@ module ApplicationHelper
     def absence_duration(absence)
         @sweeknumber = absence.start_time.cweek.to_i()
         @eweeknumber = absence.end_time.cweek.to_i()
+        @period = @eweeknumber - @sweeknumber
 
-        case
-        when @eweeknumber - @sweeknumber == 0
-            @period = TimeDifference.between(absence.start_time, absence.end_time).in_days
-            @period = @period +1
-        when @eweeknumber - @sweeknumber == 1
-            @period = TimeDifference.between(absence.start_time, absence.end_time).in_days
-            @period = @period -1
-        when @eweeknumber - @sweeknumber == 2
-            @period = TimeDifference.between(absence.start_time, absence.end_time).in_days
-            @period = @period -3
-        when @eweeknumber - @sweeknumber == 3
-            @period = TimeDifference.between(absence.start_time, absence.end_time).in_days
-            @period = @period -5
-        when @eweeknumber - @sweeknumber == 3
-            @period = TimeDifference.between(absence.start_time, absence.end_time).in_days
-            @period = @period -7
-        when @eweeknumber - @sweeknumber == 4
-            @period = TimeDifference.between(absence.start_time, absence.end_time).in_days
-            @period = @period -9
-        when @eweeknumber - @sweeknumber == 5
-            @period = TimeDifference.between(absence.start_time, absence.end_time).in_days
-            @period = @period -11
-        when @eweeknumber - @sweeknumber == 6
-            @period = TimeDifference.between(absence.start_time, absence.end_time).in_days
-            @period = @period -13
-        when @eweeknumber - @sweeknumber == 7
-            @period = TimeDifference.between(absence.start_time, absence.end_time).in_days
-            @period = @period -15
-        when @eweeknumber - @sweeknumber == 8
-            @period = TimeDifference.between(absence.start_time, absence.end_time).in_days
-            @period = @period -17
+        if @period = 0
+            @timeamount = TimeDifference.between(absence.start_time, absence.end_time).in_days
+            @timeamount = @timeamount +1
         else
-            1000
+            @timeamount = TimeDifference.between(absence.start_time, absence.end_time).in_days
+            @timeamount = @timeamount - ((@period * 2) -1)
         end
     end
 
@@ -70,40 +44,14 @@ module ApplicationHelper
     def disease_duration(disease)
         @sweeknumber = disease.start_time.cweek.to_i()
         @eweeknumber = disease.end_time.cweek.to_i()
+        @period = @eweeknumber - @sweeknumber
 
-        case
-        when @eweeknumber - @sweeknumber == 0
-            @period = TimeDifference.between(disease.start_time, disease.end_time).in_days
-            @period = @period +1
-        when @eweeknumber - @sweeknumber == 1
-            @period = TimeDifference.between(disease.start_time, disease.end_time).in_days
-            @period = @period -1
-        when @eweeknumber - @sweeknumber == 2
-            @period = TimeDifference.between(disease.start_time, disease.end_time).in_days
-            @period = @period -3
-        when @eweeknumber - @sweeknumber == 3
-            @period = TimeDifference.between(disease.start_time, disease.end_time).in_days
-            @period = @period -5
-        when @eweeknumber - @sweeknumber == 3
-            @period = TimeDifference.between(disease.start_time, disease.end_time).in_days
-            @period = @period -7
-        when @eweeknumber - @sweeknumber == 4
-            @period = TimeDifference.between(disease.start_time, disease.end_time).in_days
-            @period = @period -9
-        when @eweeknumber - @sweeknumber == 5
-            @period = TimeDifference.between(disease.start_time, disease.end_time).in_days
-            @period = @period -11
-        when @eweeknumber - @sweeknumber == 6
-            @period = TimeDifference.between(disease.start_time, disease.end_time).in_days
-            @period = @period -13
-        when @eweeknumber - @sweeknumber == 7
-            @period = TimeDifference.between(disease.start_time, disease.end_time).in_days
-            @period = @period -15
-        when @eweeknumber - @sweeknumber == 8
-            @period = TimeDifference.between(disease.start_time, disease.end_time).in_days
-            @period = @period -17
+        if @period = 0
+            @timeamount = TimeDifference.between(disease.start_time, disease.end_time).in_days
+            @timeamount = @timeamount +1
         else
-            1000
+            @timeamount = TimeDifference.between(disease.start_time, disease.end_time).in_days
+            @timeamount = @timeamount - ((@period * 2) -1)
         end
     end
 end
